c1daa3181a9af96d3fff3a239463ec8cd958c26b
sunotora <sunotora@gmail.com>
2018-01-25 01:14:13 +0900

    add Dollar

---
+++ TDDbyExample/Dollar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TDDbyExample
+{
+    public class Dollar
+    {
+        public int Amount { get; }
+
+        public Dollar(int amount)
+        {
+        }
+
+        public void Times(int multiplier)
+        {
+        }
+    }
+}
