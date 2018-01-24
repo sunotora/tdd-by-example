5aeaf93e6193aec39cb9fd800c08a749f5c86337
sunotora <sunotora@gmail.com>
2018-01-25 01:15:14 +0900

    red to green

--- TDDbyExample/Dollar.cs
+++ TDDbyExample/Dollar.cs
@@ -4,7 +4,7 @@ namespace TDDbyExample
 {
     public class Dollar
     {
-        public int Amount { get; }
+        public int Amount { get; } = 10;
 
         public Dollar(int amount)
         {
