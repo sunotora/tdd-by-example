0164525d1c48af7bcd69b08cf68056017e1f33a1
sunotora <sunotora@gmail.com>
2018-01-25 01:07:39 +0900

    add first test

---
+++ TDDbyExample.Tests/MoneyTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDbyExample.Tests
+{
+    [TestFixture]
+    public class MoneyTest
+    {
+        [Test]
+        public void 掛け算できる()
+        {
+            var five = new Dollar(5);
+            five.Times(2);
+            Assert.That(five.Amount, Is.EqualTo(10));
+        }
+    }
+}
