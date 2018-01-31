6def657fabdf4e07503291df17dfd7bed484c127
sunotora <sunotora@gmail.com>
2018-02-01 02:48:59 +0900

    Pairクラスを追加 #P105
    ハッシュテーブルに使いたいのでEqualsとGetHashCodeメソッドを実装した
    Pairクラスのテストを書かないのはリファクタリングの過程としてPairクラスを
    抽出している途中だからリファクタリングが完了してすべてのテストが通るよう
    になったらPairクラスも同時にテストされていると見なしてよい
    不安な場合はその限りではなくPairクラスのテストを書き始めてもよい

---
+++ TDDbyExample/Pair.cs
@@ -0,0 +1,25 @@
+﻿namespace TDDbyExample
+{
+    class Pair
+    {
+        string from;
+        string to;
+        public Pair(string from, string to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Pair;
+            if (other == null) return false;
+            return from == other.from && to == other.to;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+    }
+}
