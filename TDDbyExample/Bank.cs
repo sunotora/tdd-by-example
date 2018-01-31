aa9ac19435460af5f9fdea00149083f7080d93f4
sunotora <sunotora@gmail.com>
2018-02-01 02:55:12 +0900

    AddRate呼び出し時にレートを追加するようにした

--- TDDbyExample/Bank.cs
+++ TDDbyExample/Bank.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TDDbyExample
 {
     public class Bank
     {
+        Dictionary<Pair, int> rates = new Dictionary<Pair, int>();
         public Money Reduce(Expression source, string to)
         {
             return source.Reduce(this, to);
@@ -11,6 +13,7 @@ namespace TDDbyExample
 
         public void AddRate(string from, string to, int rate)
         {
+            rates.Add(new Pair(from, to), rate);
         }
 
         public int Rate(string from, string to)
