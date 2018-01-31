b8c605b34fdc66e0ac9f33f8fba9e8a15b356cf7
sunotora <sunotora@gmail.com>
2018-02-01 01:11:42 +0900

    とりあえずグリーンにした

--- TDDbyExample/Bank.cs
+++ TDDbyExample/Bank.cs
@@ -6,6 +6,8 @@ namespace TDDbyExample
     {
         public Money Reduce(Expression source, string to)
         {
+            var money = source as Money;
+            if (money != null) return money;
             var sum = (Sum)source;
             return sum.Reduce(to);
         }
