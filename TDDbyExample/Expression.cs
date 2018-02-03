4af92f0e7da5e0aeacbdd9d8dd4176993d350a7f
sunotora <sunotora@gmail.com>
2018-02-04 01:18:31 +0900

    Timesメソッドをインターフェースに引き上げてSumクラスに実装した

--- TDDbyExample/Expression.cs
+++ TDDbyExample/Expression.cs
@@ -3,6 +3,7 @@
     public interface Expression
     {
         Expression Plus(Expression addend);
+        Expression Times(int multiplier);
         Money Reduce(Bank bank, string to);
     }
 }
--- TDDbyExample/Sum.cs
+++ TDDbyExample/Sum.cs
@@ -18,10 +18,16 @@ namespace TDDbyExample
             return new Sum(this, addend);
         }
 
+        public Expression Times(int multiplier)
+        {
+            return new Sum(Augend.Times(multiplier), Addend.Times(multiplier));
+        }
+
         public Money Reduce(Bank bank,string to)
         {
             var amount = Augend.Reduce(bank, to).Amount + Addend.Reduce(bank, to).Amount;
             return new Money(amount, to);
         }
+
     }
 }
